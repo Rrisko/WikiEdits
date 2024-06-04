14e1a703389453883a9c07b854e588bec9ffa345
Rrisko <rhrivnak1@gmail.com>
2024-06-04 10:16:09 +0200

    New data pulled, added correlation analysis

--- code/api_calls.py
+++ code/api_calls.py
@@ -6,6 +6,7 @@ import pandas as pd
 from datetime import *
 import time as tm
 from collections import Counter
+import locale
 
 
 def get_article_name(
@@ -96,20 +97,154 @@ def get_revisions_by_month(
     return dict(Counter(revisions))
 
 
-def convert_log_to_datetime(log_str: str) -> datetime:
+russian_genitive_months = {
+    "января": "январь",
+    "февраля": "февраль",
+    "марта": "март",
+    "апреля": "апрель",
+    "мая": "май",
+    "июня": "июнь",
+    "июля": "июль",
+    "августа": "август",
+    "сентября": "сентябрь",
+    "октября": "октябрь",
+    "ноября": "ноябрь",
+    "декабря": "декабрь",
+}
+
+ukrainian_genitive_months = {
+    "січня": "Січень",
+    "лютого": "Лютий",
+    "березня": "Березень",
+    "квітня": "Квітень",
+    "травня": "Травень",
+    "червня": "Червень",
+    "липня": "Липень",
+    "серпня": "Серпень",
+    "вересня": "Вересень",
+    "жовтня": "Жовтень",
+    "листопада": "Листопад",
+    "грудня": "Грудень",
+}
+
+arabic_to_english_months = {
+    "يناير": "January",
+    "فبراير": "February",
+    "مارس": "March",
+    "أبريل": "April",
+    "مايو": "May",
+    "يونيو": "June",
+    "يوليو": "July",
+    "أغسطس": "August",
+    "سبتمبر": "September",
+    "أكتوبر": "October",
+    "نوفمبر": "November",
+    "ديسمبر": "December",
+}
+
+
+def convert_log_to_datetime(log_str: str, lang="en") -> datetime:
     """
     Helper function to retrieve datetime limits from protection logs
     """
 
+    format_time = "%H:%M, %d %B %Y"
+
+    if lang == "en":
+        locale.setlocale(locale.LC_ALL, "en_US")
+        log_str = log_str.replace("expires ", "")
+
+    elif lang == "de":
+        locale.setlocale(locale.LC_ALL, "de_DE")
+        log_str = log_str.replace("bis ", "")
+        log_str = log_str.replace(" Uhr", "")
+
+        format_time = "%d. %B %Y, %H:%M"
+
+    elif lang == "ru":
+        locale.setlocale(locale.LC_ALL, "ru_RU")
+        log_str = log_str.replace("истекает ", "")
+
+        log_str = re.sub(
+            r"\b" + r"|\b".join(russian_genitive_months) + r"\b",
+            lambda m: russian_genitive_months.get(m.group(), m.group()),
+            log_str,
+        )
+
+    elif lang == "uk":
+        locale.setlocale(locale.LC_ALL, "uk_UA")
+        log_str = log_str.replace("закінчується ", "")
+
+        log_str = re.sub(
+            r"\b" + r"|\b".join(ukrainian_genitive_months) + r"\b",
+            lambda m: ukrainian_genitive_months.get(m.group(), m.group()),
+            log_str,
+        )
+
+    elif lang == "ar":
+        locale.setlocale(locale.LC_ALL, "ar")
+        log_str = log_str.replace("تنتهي في ", "")
+        log_str = log_str.replace("،", ",")
+        log_str = re.sub(
+            r"\b" + r"|\b".join(arabic_to_english_months) + r"\b",
+            lambda m: arabic_to_english_months.get(m.group(), m.group()),
+            log_str,
+        )
+        locale.setlocale(locale.LC_ALL, "en_US")
+        format_time = "%H:%M, %d %b %Y"
+
+    elif lang == "sk":
+        locale.setlocale(locale.LC_ALL, "sk")
+        log_str = log_str.replace("vyprší o ", "")
+        format_time = "%H:%M, %d. %B %Y"
+
+    elif lang == "pl":
+        locale.setlocale(locale.LC_ALL, "pl")
+        log_str = log_str.replace("wygasa ", "")
+        format_time = "%H:%M, %d %b %Y"
+
+    elif lang == "cs":
+        locale.setlocale(locale.LC_ALL, "cs")
+        log_str = log_str.replace("vyprší v ", "")
+        format_time = "%d. %m. %Y, %H:%M"
+
+    elif lang == "it":
+        locale.setlocale(locale.LC_ALL, "it")
+        log_str = log_str.replace("scade il ", "")
+        log_str = log_str.replace("scade ", "")
+        log_str = log_str.replace("alle ", "")
+        format_time = "%d %b %Y %H:%M"
+
     p = r"\(([^\)]+)\)"
     match = re.search(p, log_str).group(1)
 
-    if match == "indefinite":
+    if (
+        match == "indefinite"
+        or match == "unbeschränkt"
+        or match == "безстроково"
+        or match == "غير محدد"
+        or match == "бессрочно"
+        or match == "na neurčito"
+        or match == "do odvolání"
+        or match == "na zawsze"
+        or match == "infinito"
+    ):
         return datetime(2029, 12, 31, 23, 59, 59)
 
-    date_string = match[8:-5]  # remove expires and UTC substrings
+    match = match.replace(" (UTC", "")
 
-    date_dt = datetime.strptime(date_string, "%H:%M, %d %B %Y")
+    date_string = match  # [8:-5]  # remove expires and UTC substrings
+
+    format_times = ["%H:%M, %d %B %Y", format_time, "%H:%M, %d %b %Y"]
+    for f in format_times:
+        try:
+            date_dt = datetime.strptime(date_string, f)
+            break
+        except:
+            next
+
+    locale.setlocale(locale.LC_ALL, "en_US")
+
     return date_dt
 
 
@@ -151,25 +286,45 @@ def get_protection(article: str, lang: str = "en") -> list:
         return output_list  # no protection log
 
     for log in protections["logevents"]:
-
+        print(log)
         protection_timestamp = datetime.strptime(log["timestamp"], "%Y-%m-%dT%H:%M:%SZ")
+
         if lang == "en":
-            protection_limit = convert_log_to_datetime(
-                log["params"]["description"]
-            )  # need to retrieve substring
+            try:
+                if log["params"] != {}:
+                    protection_limit = convert_log_to_datetime(
+                        log["params"]["description"]
+                    )  # need to retrieve substring
+                elif log["comment"] != {}:
+                    protection_limit = convert_log_to_datetime(
+                        log["comment"]
+                    )  # need to retrieve substring
+            except:
+                continue
 
         else:
-            protection_limit = log["params"]["details"][0][
-                "expiry"
-            ]  # found in the json structure
+            try:
+                protection_limit = log["params"]["details"][0][
+                    "expiry"
+                ]  # found in the json structure
 
-            if protection_limit == "infinite":
-                protection_limit = datetime(2029, 12, 31, 23, 59, 59)
+                if protection_limit == "infinite":
+                    protection_limit = datetime(2029, 12, 31, 23, 59, 59)
 
-            else:
-                protection_limit = datetime.strptime(
-                    protection_limit, "%Y-%m-%dT%H:%M:%SZ"
-                )
+                else:
+                    protection_limit = datetime.strptime(
+                        protection_limit, "%Y-%m-%dT%H:%M:%SZ"
+                    )
+
+            except KeyError as k:
+                if "description" in log["params"].keys():
+
+                    protection_limit = convert_log_to_datetime(
+                        log["params"]["description"], lang
+                    )
+
+                else:
+                    continue
 
         output_list.append(
             {
@@ -189,4 +344,4 @@ if __name__ == "__main__":
     # print(a)
     # print(get_revisions_by_month(a, lang="sk", timeStop=datetime(2023, 11, 1)))
     # print(get_protection("Shawarma", "en"))
-    print(get_revisions_by_month("Vienna", f="reverted"))
+    print(get_protection("Stonewall_Jackson", "en"))
